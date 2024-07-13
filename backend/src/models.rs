5cd0b5a6d83d0f32e8ef012a0cce00639f922ce0
binkybarnes <132546955+binkybarnes@users.noreply.github.com>
2024-07-13 02:22:19 -0700

    added hashed password to signup route

--- backend/src/models.rs
+++ backend/src/models.rs
@@ -1,13 +1,18 @@
 // use rocket::serde::{json::Json, Deserialize, Serialize};
+use once_cell::sync::Lazy;
+use regex::Regex;
 use rocket::serde::{Deserialize, Serialize};
+
 use validator::{Validate, ValidationError};
+
+static ALPHA_NUM: Lazy<Regex> = Lazy::new(|| Regex::new(r"^[a-zA-Z0-9_ඞ]+$").unwrap());
 
 #[derive(Deserialize, Validate)]
 pub struct UserSignupInput<'r> {
     // email, username, password, confirmPassword
     #[validate(email)]
     pub email: &'r str,
-    #[validate(length(min = 1, max = 15))]
+    #[validate(length(min = 1, max = 15), regex(path = *ALPHA_NUM))]
     pub username: &'r str,
     #[validate(length(min = 1, max = 20))]
     pub password: &'r str,
