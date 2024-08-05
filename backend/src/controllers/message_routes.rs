fac9a498ce21406ffd9343afb634b7071b761cab
binkybarnes <132546955+binkybarnes@users.noreply.github.com>
2024-08-04 21:04:36 -0700

    finished get message and check user in server function

--- backend/src/controllers/message_routes.rs
+++ backend/src/controllers/message_routes.rs
@@ -16,26 +16,29 @@ pub async fn send_message(
     mut db: Connection<database::HarmonyDb>,
 ) -> impl Responder {
     let user_id = &guard.0.sub;
-
+    let channel_id = message.channel_id;
     // check if user is in the server that has the channel the message is being sent to
-    let server_id = sqlx::query_scalar!(
-        "SELECT server_id FROM channels WHERE channel_id = $1",
-        message.channel_id
-    )
-    .fetch_optional(&mut **db)
-    .await
-    .map_err(|_| (Status::BadRequest, "database error"))?
-    .ok_or((Status::NotFound, "channel not found"))?;
 
-    sqlx::query_scalar!(
-        "SELECT 1 FROM users_servers WHERE user_id = $1 AND server_id = $2",
-        user_id,
-        server_id
-    )
-    .fetch_optional(&mut **db)
-    .await
-    .map_err(|_| (Status::BadRequest, "database error"))?
-    .ok_or((Status::Forbidden, "user not in server"))?;
+    user_in_server(&mut db, channel_id, *user_id).await?;
+
+    // let server_id = sqlx::query_scalar!(
+    //     "SELECT server_id FROM channels WHERE channel_id = $1",
+    //     message.channel_id
+    // )
+    // .fetch_optional(&mut **db)
+    // .await
+    // .map_err(|_| (Status::BadRequest, "database error"))?
+    // .ok_or((Status::NotFound, "channel not found"))?;
+
+    // sqlx::query_scalar!(
+    //     "SELECT 1 FROM users_servers WHERE user_id = $1 AND server_id = $2",
+    //     user_id,
+    //     server_id
+    // )
+    // .fetch_optional(&mut **db)
+    // .await
+    // .map_err(|_| (Status::BadRequest, "database error"))?
+    // .ok_or((Status::Forbidden, "user not in server"))?;
 
     // if user_in_server.is_none() {
     //     return Err((Status::Forbidden, "user not in server"));
@@ -61,18 +64,57 @@ pub async fn send_message(
     Ok::<_, (Status, &str)>(status::Created::new("/send").body(response))
 }
 
-// #[get("/get", format = "json", data = "<channel>")]
-// pub async fn get_messages(guard: JwtGuard, channel: Json<models::GetMessagesInput>) {
+// gets all messages from a channel
+#[get("/get", format = "json", data = "<channel>")]
+pub async fn get_messages(
+    guard: JwtGuard,
+    channel: Json<models::GetMessagesInput>,
+    mut db: Connection<database::HarmonyDb>,
+) -> Result<Json<Vec<models::Message>>, (Status, &'static str)> {
+    // check if channel exists
+    let channel_id = channel.channel_id;
+    let user_id = &guard.0.sub;
 
-//     // check if channel exists
-//     let channel_id = channel.channel_id;
+    user_in_server(&mut db, channel_id, *user_id).await?;
 
-//     sqlx::query_scala!(
-//         "SELECT 1 FROM channels WHERE channel_id = $1"
-//     )
+    let messages = sqlx::query_as!(
+        models::Message,
+        "SELECT * FROM messages WHERE channel_id = $1",
+        channel_id
+    )
+    .fetch_all(&mut **db)
+    .await
+    .map_err(|_| (Status::BadRequest, "database error"))?;
 
-//     sqlx::query!(
-//         "SELECT * FROM messages WHERE channel_id = $1",
-//         channel_id
-//     )
-// }
+    Ok(Json(messages))
+}
+
+// check user membership in a server
+async fn user_in_server(
+    db: &mut Connection<database::HarmonyDb>,
+    channel_id: i32,
+    user_id: i32,
+) -> Result<(), (Status, &'static str)> {
+    // Fetch server_id from channel
+    let server_id = sqlx::query_scalar!(
+        "SELECT server_id FROM channels WHERE channel_id = $1",
+        channel_id
+    )
+    .fetch_optional(&mut ***db)
+    .await
+    .map_err(|_| (Status::InternalServerError, "database error"))?
+    .ok_or((Status::NotFound, "channel not found"))?;
+
+    // Check if the user is in the server
+    sqlx::query_scalar!(
+        "SELECT 1 FROM users_servers WHERE user_id = $1 AND server_id = $2",
+        user_id,
+        server_id
+    )
+    .fetch_optional(&mut ***db)
+    .await
+    .map_err(|_| (Status::InternalServerError, "database error"))?
+    .ok_or((Status::Forbidden, "user not in server"))?;
+
+    Ok(())
+}
