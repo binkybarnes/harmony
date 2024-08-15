2e6d1e7ade3e59848a2b6eca57133dc525a07e15
binkybarnes <132546955+binkybarnes@users.noreply.github.com>
2024-08-14 22:00:25 -0700

    scrollbar for firefox and webkit browsers started

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -2,7 +2,7 @@ import Message from "./Message";
 
 const Messages = () => {
   return (
-    <div className="flex-1 overflow-auto rounded-md bg-cyan-400">
+    <div className="scrollbar-messages flex-1 overflow-y-scroll rounded-md bg-cyan-400 pb-4">
       <Message />
       <Message />
       <Message />
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,7 +1,7 @@
 import Conversation from "./Conversation";
 const Conversations = () => {
   return (
-    <div className="flex flex-1 flex-col gap-1 overflow-y-scroll pl-2 scrollbar">
+    <div className="scrollbar-conversations flex flex-1 flex-col gap-1 overflow-y-scroll pl-2">
       <Conversation />
       <Conversation />
       <Conversation />
